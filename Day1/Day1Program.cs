0e364ce791b522f75877f6a399e2070b2d98eb1f
Brian Pierson <bcpierson@bsu.edu>
2019-12-02 02:40:16 -0500

    Set up project/Day 1 code

---
+++ Day1/Day1Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeneralUsage;
+
+
+namespace AdventOfCode2019.Day1
+{
+    class Day1Problem1
+    {
+       
+        public static void Main(string[] args)
+        {
+            Problem2();
+        }
+
+        // https://adventofcode.com/2019/day/1
+        private static void Problem1()
+        {
+            List<String> moduleMasses = FileImporter.Import("Problem1Input");
+            int totalMass = 0;
+            foreach (string massStr in moduleMasses)
+            {
+                double mass = Int32.Parse(massStr);
+                int fuelReq = Convert.ToInt32(Math.Floor(mass / 3) - 2);
+                totalMass += fuelReq;
+            }
+            Console.WriteLine(totalMass);
+            Console.ReadKey(true);
+        }
+
+        // https://adventofcode.com/2019/day/1#part2
+        private static void Problem2()
+        {
+            List<String> moduleMasses = FileImporter.Import("Problem1Input");
+            int totalMass = 0;
+            foreach (string massStr in moduleMasses)
+            {
+                double mass = Int32.Parse(massStr);
+                totalMass += calculateNeededFuel(mass);
+            }
+            Console.WriteLine(totalMass);
+            Console.ReadKey(true);
+        }
+
+        private static int calculateNeededFuel(double inputMass) //yay recursion
+        {
+            int neededFuel = 0;
+            if (inputMass > 0)
+            {
+                int fuelReq = Convert.ToInt32(Math.Floor(inputMass / 3) - 2);
+                neededFuel = fuelReq;
+                int fuelForFuel = calculateNeededFuel(fuelReq);
+                if (fuelForFuel >= 0) { neededFuel += fuelForFuel; }
+            }           
+            return neededFuel;
+        }
+    }
+}
--- GeneralUsage/FileImporter.cs
+++ GeneralUsage/FileImporter.cs
@@ -1,19 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AdventOfCode2019
+namespace GeneralUsage
 {
-    class FileImporter
+    public class FileImporter
     {
-
-       public static List<string> Import(string file)
+        public static void Main(string[] args)
         {
-            string path = "C:\\Users\\14pie\\source\\repos\\AdventOfCode2019\\AdventOfCode2019\\Data\\" + file + ".txt";
+            //so Visual Studio dosen't yell at me
+            //yes, there are better ways to set this up than I have done
+        }
+
+
+        public static List<string> Import(string file)
+        {
+            string path = "C:\\Users\\14pie\\source\\repos\\AdventOfCode2019\\GeneralUsage\\Data\\" + file + ".txt";
             //string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\"+file+".txt");     make dynamic later
             string[] lines = System.IO.File.ReadAllLines(path);
             List<string> lineList = new List<string>(lines);
