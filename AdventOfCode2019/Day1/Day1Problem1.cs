6cad9daa4db9abef7fe29850035e0218126f3cbb
Brian Pierson <bcpierson@bsu.edu>
2019-12-02 01:48:53 -0500

    Add project files.

---
+++ AdventOfCode2019/Day1/Day1Problem1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AdventOfCode2019.Day1
+{
+    class Day1Problem1
+    {
+        // https://adventofcode.com/2019/day/1
+        public static void Main(string[] args)
+        {
+            List<String> moduleMasses = FileImporter.Import("Problem1Input");
+            int totalMass = 0;
+            foreach (string massStr in moduleMasses)
+            {
+                double mass = Int32.Parse(massStr);
+                int fuelReq = Convert.ToInt32(Math.Floor(mass/3) - 2);
+                totalMass += fuelReq;
+            }
+            Console.WriteLine(totalMass);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ AdventOfCode2019/FileImporter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2019
+{
+    class FileImporter
+    {
+
+       public static List<string> Import(string file)
+        {
+            string path = "C:\\Users\\14pie\\source\\repos\\AdventOfCode2019\\AdventOfCode2019\\Data\\" + file + ".txt";
+            //string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\"+file+".txt");     make dynamic later
+            string[] lines = System.IO.File.ReadAllLines(path);
+            List<string> lineList = new List<string>(lines);
+            return lineList;
+        }
+    }
+}
